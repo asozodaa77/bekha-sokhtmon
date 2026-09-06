d1b1a6b054ddda529cc6ffa93f67026688af1837
asozodaa77 <asozodabehruz77@gmail.com>
2026-09-06 14:22:47 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-  const navToggle = document.getElementById('navToggle');
-  const navMenu = document.getElementById('navMenu');
-  navToggle.addEventListener('click', () => navMenu.classList.toggle('open'));
-  document.querySelectorAll('#navMenu a').forEach(a => a.addEventListener('click', () => navMenu.classList.remove('open')));
